03fb6937efe83e573b727f232f79ad1e4c11beae
Thamiris Balbi <thamirisbalbi.estudante@gmail.com>
2022-04-04 10:29:18 -0300

    Tira a visibilidade do cabeçalho

--- src/routes.js
+++ src/routes.js
@@ -10,10 +10,12 @@ import Detail from './pages/Detail';
 export default function Routes() {
     return (
         <NavigationContainer>
-            <AppStack.Navigator>
+
+            <AppStack.Navigator screenOptions={{ headerShown: false }}>
                 <AppStack.Screen name="Incidents" component={Incidents}/>
-                <AppStack.Screen name="Details" component={Detail}/>
+                <AppStack.Screen name="Detail" component={Detail}/>
             </AppStack.Navigator>
+
         </NavigationContainer>
     );
 }
