cb4ce781e7cd97674c74a45e2a09fc579a5adfc7
Thamiris Balbi <thamirisbalbi.estudante@gmail.com>
2022-04-04 10:39:03 -0300

    Importa a logo para index.js de incidents

--- src/pages/incidents/index.js
+++ src/pages/incidents/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import logoimg from '../../assets/logo.png'; //passa automaticamente a logo que se adequa a tela que roda a aplicação
+
 export default function Incidents() {
     return (
         <View />
