b4cbb4ce44a171dfe6f1bb84563d3869a810c0bf
Thamiris Balbi <thamirisbalbi.estudante@gmail.com>
2022-04-04 12:51:57 -0300

    Estrutura header da página detail

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { View, TouchableOpacity } from 'react-native';
+
+import logoimg from '../../assets/logo.png';
+
+import styles from './styles';
 
 export default function Detail() {
     return (
-        <View />
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logoimg} />
+                
+                <TouchableOpacity onPress={() => {}}>
+                    <Feather name="arrow-left" size={28} color="#E02041" />
+                </TouchableOpacity>
+            </View>
+        </View>
     );
 }
