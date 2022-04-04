106233eb8c2bae236729fa5e15a9554c4e1b06e4
Thamiris Balbi <thamirisbalbi.estudante@gmail.com>
2022-04-04 11:49:34 -0300

    Estiliza botão da lista de casos

--- src/pages/incidents/styles.js
+++ src/pages/incidents/styles.js
@@ -61,4 +61,16 @@ export default StyleSheet.create({
         marginBottom: 24,
         color: '#737380'
     },
+
+    detailsButton: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+
+    detailsButtonText: {
+        color: '#e02041',
+        fontSize: 15,
+        fontWeight: 'bold'
+    },
 });
