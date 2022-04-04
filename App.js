b7d1abf8be0fbcb2680cf6f8fc76204b0654d73a
Thamiris Balbi <thamirisbalbi.estudante@gmail.com>
2022-04-04 10:23:59 -0300

    Importa rotas para app.js e tira outras aplicações

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
+import Routes from './src/routes';
+
 export default function App() {
   return (
-    <View>
-      <Text>Hello Omnistack</Text>
-    </View>
+    <Routes />
   );
 }
 
