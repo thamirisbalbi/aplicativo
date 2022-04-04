625f47bf27de21b242410cb780d1c481e2ae45a0
Thamiris Balbi <thamirisbalbi.estudante@gmail.com>
2022-04-04 11:01:14 -0300

    Estiliza a header da página incidents

--- src/pages/incidents/styles.js
+++ src/pages/incidents/styles.js
@@ -1,5 +1,26 @@
 import { StyleSheet } from "react-native";
+import { Constants } from "expo-constants"; //importa informações fixas para cada projeto
 
 export default StyleSheet.create({
-    container: {}
+    container: {
+        flex: 1, //aplicação ocupa tamanho inteiro
+        paddingHotizontal: 24, //quero fazer um padding nas laterais
+        paddingTop: Constants.statusBarHeight + 20, //tamanho da status bar para ela ficar mais abaixo da tela
+
+    },
+
+    header: {
+        flexDirection: 'row', //os itens do header ficam lado a lado
+        justifyContent: 'space-between',
+        alignItems: 'center' //centraliza o texto e o coloca à direita  
+    },
+
+    headerText: {
+        fontSize: 15,
+        color: '#737380',
+    }, 
+
+    headerTextBold: {
+        fontWeight: 'bold'
+    }
 });
