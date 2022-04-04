87c6a737c461ba3bc6cd20e3c5f7596602daf317
Thamiris Balbi <thamirisbalbi.estudante@gmail.com>
2022-04-04 10:41:44 -0300

    importa os estilos para index.js

--- src/pages/incidents/index.js
+++ src/pages/incidents/index.js
@@ -3,6 +3,8 @@ import { View } from 'react-native';
 
 import logoimg from '../../assets/logo.png'; //passa automaticamente a logo que se adequa a tela que roda a aplicação
 
+import styles from './styles';
+
 export default function Incidents() {
     return (
         <View />
