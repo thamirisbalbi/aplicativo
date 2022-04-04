efb626b1c8c50f02b040da05f8ebc170b4d43c1f
Thamiris Balbi <thamirisbalbi.estudante@gmail.com>
2022-04-04 12:52:55 -0300

    Importa aplicação de imagem

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
-import { View, TouchableOpacity } from 'react-native';
+import { View, Image, TouchableOpacity } from 'react-native';
 
 import logoimg from '../../assets/logo.png';
 
@@ -16,6 +16,8 @@ export default function Detail() {
                     <Feather name="arrow-left" size={28} color="#E02041" />
                 </TouchableOpacity>
             </View>
+
+
         </View>
     );
 }
