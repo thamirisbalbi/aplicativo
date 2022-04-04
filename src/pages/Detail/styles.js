c2c7e9a9bdb86902f95e6a581f9d41d17dd53cce
Thamiris Balbi <thamirisbalbi.estudante@gmail.com>
2022-04-04 13:12:43 -0300

    Estiliza header de detail

--- src/pages/Detail/styles.js
+++ src/pages/Detail/styles.js
@@ -7,5 +7,9 @@ export default StyleSheet.create({
         paddingTop: Constants.statusBarHeight + 20,
     },
 
-    
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
 });
