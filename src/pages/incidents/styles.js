36129a327211c8d43a32a672e7d2639b2e81548c
Thamiris Balbi <thamirisbalbi.estudante@gmail.com>
2022-04-04 11:45:47 -0300

    Estilização de incidentList e incidents

--- src/pages/incidents/styles.js
+++ src/pages/incidents/styles.js
@@ -38,4 +38,27 @@ export default StyleSheet.create({
         color: '#737380'
     },
 
+    incidentList: {
+        marginTop: 32,
+    },
+
+    incident: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#FFF',
+        marginBottom: 16,
+    },
+
+    incidentProperty: {
+        fontSize: 14,
+        color: '#41414d',
+        fontWeight: 'bold'
+    },
+
+    incidentValue: {
+        marginTop: 8,
+        fontSize: 15,
+        marginBottom: 24,
+        color: '#737380'
+    },
 });
