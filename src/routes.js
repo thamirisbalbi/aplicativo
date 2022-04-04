3af7907859ab5f016e2cb1dbd8df40a20b0c9c57
Thamiris Balbi <thamirisbalbi.estudante@gmail.com>
2022-04-04 10:22:27 -0300

    Estruturação da página de rotas

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import { NavigationContainer } from '@react-navigation/native';
+import {} from '@react-navigation/stack';
+
+const AppStack = createStackNavigator(); //primeira navegação criada
+
+import Incidents from './pages/incidents';
+import Detail from './pages/Detail';
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <AppStack.Navigator>
+                <AppStack.Screen component={Incidents}/>
+                <AppStack.Screen component={Detail}/>
+            </AppStack.Navigator>
+        </NavigationContainer>
+    );
+}
