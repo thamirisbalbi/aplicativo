d6c73e0bdbfaa013343daaaea08be89cd7f99d0e
Thamiris Balbi <thamirisbalbi.estudante@gmail.com>
2022-04-04 12:34:19 -0300

    Esconde scrol vertical

--- src/pages/incidents/index.js
+++ src/pages/incidents/index.js
@@ -23,6 +23,7 @@ export default function Incidents() {
                 data={[1, 2, 3]}
                 style={styles.incidentList}
                 keyExtractor={incident => String(incident)}
+                showsVerticalScrollIndicator={false}
                 renderItem={() => (
                     <View style={styles.incident}>
                         <Text style={styles.incidentProperty}>ONG:</Text>
