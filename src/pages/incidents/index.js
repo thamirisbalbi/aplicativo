c22aa0c843631e7256f4cc61a5d44ddf01ae972a
Thamiris Balbi <thamirisbalbi.estudante@gmail.com>
2022-04-04 10:47:36 -0300

    Estruturação de header em index.js

--- src/pages/incidents/index.js
+++ src/pages/incidents/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, Text } from 'react-native';
 
 import logoimg from '../../assets/logo.png'; //passa automaticamente a logo que se adequa a tela que roda a aplicação
 
@@ -7,6 +7,13 @@ import styles from './styles';
 
 export default function Incidents() {
     return (
-        <View />
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logoimg} />
+                <Text style={styles.headerText}>
+                    Total de <Text style={styles.headerTextBold}>0 casos</Text>.
+                </Text>
+            </View>
+        </View>
     );
 }
