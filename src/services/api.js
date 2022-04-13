e77513f0979f2996ce66aae31c99ae1c50d1ff6b
Thamiris Balbi <thamirisbalbi.estudante@gmail.com>
2022-04-12 23:32:59 -0300

    expo rodando em tunnel

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.15.169:3333' //endereço ip + porta usada no node
+    baseURL: 'https://new-skunk-73.loca.lt' //endereço ip + porta usada no node
 });
 
 export default api;
